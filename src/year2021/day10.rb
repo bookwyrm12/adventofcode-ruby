bfda971c8a7ef26ba3054e8f32204380d0365fe4
April <bookwyrm12@users.noreply.github.com>
2021-12-09 23:36:55 -0800

    refactor 2021 day 10 to use class constants

--- src/year2021/day10.rb
+++ src/year2021/day10.rb
@@ -1,12 +1,22 @@
 module Year2021
   class Day10
+    CHAR_MAP = {')': '(', ']': '[', '}': '{', '>': '<'}
+    PUSH_CHARS = ['(', '[', '{', '<']
+    POP_CHARS = [')', ']', '}', '>']
+    ILLEGAL_CHAR_SCORES = {
+      ')': 3,
+      ']': 57,
+      '}': 1197,
+      '>': 25137,
+    }
+
     def part1(input)
       input = parse_input(input)
 
       syntax_error_score = 0
       input.each do |row|
         status, invalid_char = parse_line(row)
-        syntax_error_score += syntax_scores[invalid_char.to_sym] if status == :corrupted
+        syntax_error_score += ILLEGAL_CHAR_SCORES[invalid_char.to_sym] if status == :corrupted
       end
 
       syntax_error_score
@@ -22,30 +32,18 @@ module Year2021
     end
 
     def parse_line(line)
-      char_map = {')': '(', ']': '[', '}': '{', '>': '<'}
-      push_chars = ['(', '[', '{', '<']
-      pop_chars = [')', ']', '}', '>']
       line_stack = []
       line.each_char do |char|
-        if push_chars.include?(char)
+        if PUSH_CHARS.include?(char)
           line_stack.push(char)
-        else #if pop_chars.include?(char)
+        else #if POP_CHARS.include?(char)
           matching_char = line_stack.pop
-          return :corrupted, char if matching_char != char_map[char.to_sym]
+          return :corrupted, char if matching_char != CHAR_MAP[char.to_sym]
         end
       end
 
       return :incomplete, nil if line_stack.size > 0
       return :success, nil
-    end
-
-    def syntax_scores
-      {
-        ')': 3,
-        ']': 57,
-        '}': 1197,
-        '>': 25137,
-      }
     end
   end
 end
