1a4a6fd095e85c7e7b3e9a83889f64aee915d845
April <bookwyrm12@users.noreply.github.com>
2022-12-04 01:47:25 -0800

    2022 day 4 part 1

---
+++ src/year2022/day04.rb
@@ -0,0 +1,35 @@
+module Year2022
+  class Day04
+    def part1(input)
+      input = parse_input(input)
+      fully_contains = 0
+      input.each do |pair|
+        if fully_contains?(pair)
+          fully_contains += 1
+        end
+      end
+      return fully_contains
+    end
+
+    def part2(input)
+      nil
+    end
+
+    # Helpers
+    def parse_input(input)
+      input.split("\n").map do |pair|
+        result = pair.match(/(\d+)-(\d+),(\d+)-(\d+)/)
+        { elf1_start: result[1].to_i, elf1_end: result[2].to_i, elf2_start: result[3].to_i, elf2_end: result[4].to_i }
+      end
+    end
+
+    def fully_contains?(pair)
+      if (pair[:elf1_start] <= pair[:elf2_start] && pair[:elf1_end] >= pair[:elf2_end]) ||
+        (pair[:elf1_start] >= pair[:elf2_start] && pair[:elf1_end] <= pair[:elf2_end])
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
